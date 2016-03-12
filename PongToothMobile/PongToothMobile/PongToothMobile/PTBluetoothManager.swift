82d2452fb19e3b4afa0121816d08cf10796360f6
efurnon <124975@supinfo.com>
2016-03-12 16:29:04 +0100

    Create project mobile

---
+++ PongToothMobile/PongToothMobile/PongToothMobile/PTBluetoothManager.swift
@@ -0,0 +1,9 @@
+//
+//  PTBluetoothManager.swift
+//  PongToothMobile
+//
+//  Created by Emmanuel Furnon on 12/03/2016.
+//  Copyright © 2016 Emmanuel Furnon. All rights reserved.
+//
+
+import Foundation
