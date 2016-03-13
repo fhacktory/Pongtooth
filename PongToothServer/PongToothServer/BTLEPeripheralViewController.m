80ba3024b9580ba601bbab6e821196e89d535c91
Gabriel Bremond <gabriel.bremond@keyrus.com>
2016-03-13 09:31:00 +0100

    Deal with it.

--- PongToothServer/PongToothServer/BTLEPeripheralViewController.m
+++ PongToothServer/PongToothServer/BTLEPeripheralViewController.m
@@ -61,7 +61,7 @@
     // ... so build our service.
     
     // Start with the CBMutableCharacteristic
-    self.transferCharacteristic = [[CBMutableCharacteristic alloc] initWithType:[CBUUID UUIDWithString:@"E20A39F4-73F5-4BC4-A12F-17D1AD07A961"]
+    self.transferCharacteristic = [[CBMutableCharacteristic alloc] initWithType:[CBUUID UUIDWithString:@"08590F7E-DB05-467E-8757-72F6FAEB13D4"]
                                                                       properties:CBCharacteristicPropertyNotify
                                                                            value:nil
                                                                      permissions:CBAttributePermissionsReadable];
